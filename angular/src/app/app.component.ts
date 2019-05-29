b1f380cb10eb31c09e5433e026afc7548e07b5f0
MrCerealKiller <jgomallette@gmail.com>
2019-05-29 15:41:00 -0400

    Change app-level stylesheet to scss

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html', 
-  styleUrls: ['./app.component.sass']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'AgROS';
